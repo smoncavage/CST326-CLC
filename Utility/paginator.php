081fc86356f84f3b2389736bc686b4ad6354198f
smoncavage <moncavage@hotmail.com>
2023-08-07 18:00:52 -0400

    update paginator to reduce complexity

--- Utility/paginator.php
+++ Utility/paginator.php
@@ -56,31 +56,32 @@ class Paginator{
         $start      = ( ( $this->_page - $links ) > 0 ) ? $this->_page - $links : 1;
         $end        = ( ( $this->_page + $links ) < $last ) ? $this->_page + $links : $last;
         
-        $html       = '<ul class="' . $list_class . '">';
-        
-        $class      = ( $this->_page == 1 ) ? "disabled" : "";
-        $html       .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . ( $this->_page - 1 ) . '">&laquo;</a></li>';
+        $html1       = '<ul class="' . $list_class . '">';
+       
+        $html2       .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . ( $this->_page - 1 ) . '">&laquo;</a></li>';
         
         if ( $start > 1 ) {
-            $html   .= '<li><a href="?limit=' . $this->_limit . '&page=1">1</a></li>';
-            $html   .= '<li class="disabled"><span>...</span></li>';
+            $html3   .= '<li><a href="?limit=' . $this->_limit . '&page=1">1</a></li>';
+            $html4   .= '<li class="disabled"><span>...</span></li>';
         }
         
         for ( $i = $start ; $i <= $end; $i++ ) {
             $class  = ( $this->_page == $i ) ? "active" : "";
-            $html   .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . $i . '">' . $i . '</a></li>';
+            $html5   .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . $i . '">' . $i . '</a></li>';
         }
         
         if ( $end < $last ) {
-            $html   .= '<li class="disabled"><span>...</span></li>';
-            $html   .= '<li><a href="?limit=' . $this->_limit . '&page=' . $last . '">' . $last . '</a></li>';
+            $html6   .= '<li class="disabled"><span>...</span></li>';
+            $html7   .= '<li><a href="?limit=' . $this->_limit . '&page=' . $last . '">' . $last . '</a></li>';
         }
         
         $class      = ( $this->_page == $last ) ? "disabled" : "";
-        $html       .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . ( $this->_page + 1 ) . '">&raquo;</a></li>';
+        $html8       .= '<li class="' . $class . '"><a href="?limit=' . $this->_limit . '&page=' . ( $this->_page + 1 ) . '">&raquo;</a></li>';
         
-        $html       .= '</ul>';
+        $html9       .= '</ul>';
+		
+		$htmlString = $html1 + /n + $html2 + /n + $html3 + /n + $html4 + /n + $html5 + /n $html6 + /n + $html7 + /n + $html8 + /n + $html9
         
-        return $html;
+        return $htmlString;
     }
 }
